190be9ed42fa46087f6051f8fb48638df76362b1
unknown <24523022@gmail.students.uii.ac.id>
2025-07-21 18:59:16 +0700

    PostureCare

--- TBESAR/src/log/util/AppData.java
+++
@@ -1,9 +0,0 @@
-package src.log.util;
-
-import java.util.ArrayList;
-import java.util.List;
-import src.log.model.User;
-
-public class AppData {
-    public static final List<User> userList = new ArrayList<>();
-}
